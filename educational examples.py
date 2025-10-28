5dfc749bac43d5121c6cd76882ab7a19a96859a7
5w8kpgktgn-svg <5w8kpgktgn@privaterelay.appleid.com>
2025-10-29 01:12:06 +0300

    educational example(selected examples)

---
+++ educational examples.py
@@ -0,0 +1,175 @@
+# ŞİFRE OLUŞTURMA
+
+# def parola(şifre):
+#     import re
+#     if len(şifre) < 8:
+#         raise Exception("şifre en az 8 karakterli olmalıdır")
+#     elif not re.search("[a-z]",şifre):
+#         raise Exception("şifrede küçük harf olmalıdır")
+#     elif not re.search("[A-Z]",şifre):
+#         raise Exception("şifrede büyük harf olmalıdır")
+#     elif not re.search("[0-9]",şifre):
+#         raise Exception("şifrede rakam olmalıdır")
+#     elif re.search("\s",şifre):
+#         raise Exception("şifrede boşluk olmamalıdır")
+# while True:
+#   cevap=input("lütfen şifre oluşturun")
+#   try:
+#     parola(cevap)
+#   except Exception as ex:
+#     print(ex)
+#   else:
+#       print("şifre başarılı bir şekilde oluşturuldu")
+#       break
+
+#####################################################
+# QUİZ 
+ 
+# class Soru:
+#     def __init__(self,yazi,şık,cevap):
+#         self.yazi=yazi
+#         self.şık=şık
+#         self.cevap=cevap
+#     def kontrol(self,gelen):
+#         return self.cevap.lower() == gelen.lower()
+    
+# class Quiz:
+#     def __init__(self,sorular):
+#         self.sorular=sorular
+#         self.puan=0
+#         self.index=0
+#     def sorudöndür(self):
+#         return self.sorular[self.index]
+#     def göster(self):
+#         soru=self.sorudöndür()
+#         print(f"soru {self.index + 1}: {soru.yazi}")
+#         for g in soru.şık:
+#             print(f"- {g}")
+#         alınancevap=input("cevap:")
+#         self.cevapdurumunagöreyazdırma(alınancevap)
+#     def cevapdurumunagöreyazdırma(self,alınancevap):
+#         soru=self.sorudöndür()
+#         if soru.kontrol(alınancevap):
+#             self.puan += 1
+#         else:
+#             pass
+#         self.index += 1
+#         self.devamlılık_durumu()
+#     def devamlılık_durumu(self):
+#         if self.index == len(self.sorular):
+#             print(f"puan:{self.puan}")
+#         else:
+#             self.göster()
+
+
+# s1 = Soru("En iyi programlama dili?", ["Python", "C#", "JavaScript", "Java"], "python")
+# s2 = Soru("En popüler programlama dili?", ["C#", "Python", "JavaScript", "Java"], "python")
+# s3 = Soru("En çok kazandıran programlama dili?", ["C#", "Python", "Java", "JavaScript"], "python")
+# liste=[s1,s2,s3]
+# kal=Quiz(liste)
+# kal.göster()
+###########################################################
+# FİLM
+# “Soru” sınıfı: ipucu (yazi) ve doğru cevap (dogru) 
+
+# class Soru:
+#     def __init__(self,yazi,cevap):
+#         self.yazi=yazi
+#         self.cevap=cevap
+#     def kontrol(self,gelen):
+#         return self.cevap.lower() == gelen.lower()
+    
+# class Quiz:
+#     def __init__(self,sorular):
+#         self.sorular=sorular
+#         self.puan=0
+#         self.index=0
+#     def döndür(self):
+#         return self.sorular[self.index]
+#     def göster(self):
+#         soru=self.döndür()
+#         print(f"soru {self.index + 1}: {soru.yazi}")
+#         alınancevap=input("cevap:")
+#         self.cevap_durumu(alınancevap)
+#     def cevap_durumu(self,gelen):
+#         soru=self.döndür()
+#         if soru.kontrol(gelen):
+#             self.puan += 1
+#         else:
+#             pass
+#         self.index += 1
+#         self.nereye_kadar()
+#     def nereye_kadar(self):
+#         if self.index == len(self.sorular):
+#             print(f"puan:{self.puan}")
+#         else:
+#             self.göster()
+
+# a1=Soru("size bir ip ucu verilecek ve siz filmi/diziyi tahmin edeceksiniz,ipucu:Bir adam rüyalarında bilinç altına girerek fikir çalıyor","inception")
+# a2=Soru("ipucu:Kölelikten kaçan bir adam gladyatör olur","gladiator")
+# a3=Soru("ipucu:Bir öğretmen kimya bilgisiyle uyuşturucu üretmeye başlar","breaking bad")
+# liste=[a1,a2,a3]
+# kal=Quiz(liste)
+# kal.göster()
+#############################################################
+# BANKAMATİK
+
+# class Hesap:
+#     def __init__(self,isim,bakiye,ekbakiye):
+#         self.isim=isim
+#         self.bakiye=bakiye
+#         self.ekbakiye=ekbakiye
+#     def paraçekme(self,para):
+#         if self.bakiye >= para:
+#             self.bakiye -= para
+#             print(f"Hesabınızdan {para}tl çekilmiştir")
+#         else:
+#             toplam= self.bakiye + self.ekbakiye
+#             if toplam >= para:
+#                 gelen=input("ekbakiyeyi kullanmak ister misiniz(evet/hayır):")
+#                 if gelen == "evet":
+#                     kalan= para - self.bakiye
+#                     self.bakiye=0
+#                     self.ekbakiye -= kalan
+#                     print(f"Hesabınızdan {para}tl çekilmiştir")
+#                 else:
+#                     pass
+#             else:
+#                 print(f"üzgünüz,bakiyeniz yetersiz")
+#     def bakiye_göster(self):
+#         print(f"Ana baiye:{self.bakiye},Ekbakiye:{self.ekbakiye}")
+# emirhan=Hesap("emirhan",2000,1000)
+# ali=Hesap("ali",1000,500)
+# emirhan.paraçekme(4000)
+# emirhan.bakiye_göster()
+##################################################################
+# LAMBDA,MAP VE FİLTER 
+
+# numbers=[1,3,5,9]
+# islem=list(map(lambda a: a ** 2,numbers))    
+# print(islem) 
+
+# numbers=[1,3,5,9,10,15,14,17,26]
+# ksy=list(filter(lambda kum: kum%2==0,numbers))
+# print(ksy)
+###################################################################
+# SAYI TAHMİN OYUNU
+
+# import random
+# sayı= random.randint(1,10)
+# hak=int(input("bu sayıyı kaç hakta tahmin edersiniz"))
+# puan=100
+# eksil= 100 / hak
+# i = 0
+# while i < hak:
+#     tahmin=int(input("tahnin edin:"))
+#     if tahmin >sayı:
+#         print("lütfen daha küçük bir sayı tahmin edin")
+#     elif tahmin < sayı:
+#         print("lütfen daha büyük bir sayı tahmin edin")
+#     else:
+#         print("tebrikler doğru bildiniz")
+#         break
+# i += 1    
+####################################################################
+
